9ff0ee717fef65260549b01df28d9ca158881ef0
Ditdo <ditdo@Ditdos-MacBook-Pro.local>
2021-03-09 07:27:51 -0800

    Created Boilerplate, Partials and some basic styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Cargo = require('./models/cargo');
 const { response } = require('express');
@@ -21,6 +22,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
