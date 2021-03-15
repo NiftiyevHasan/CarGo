f1b1776b6332adfd1573f4c5e3111c4839d4f936
Hasan Niftiyev <ditdo@Ditdos-MacBook-Pro.local>
2021-03-15 13:43:44 -0700

    fixed some obvious security issues

--- schemas.js
+++ schemas.js
@@ -1,13 +1,37 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.cargoSchema = Joi.object({
     cargo: Joi.object({
-        type: Joi.string().required(),
+        type: Joi.string().required().escapeHTML(),
         // image: Joi.string().required(),
         weight: Joi.number().required().min(0),
-        location: Joi.string().required(),
-        destination: Joi.string().required(),
-        description: Joi.string().required(),
+        location: Joi.string().required().escapeHTML(),
+        destination: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML(),
     }).required(),
     deleteImages: Joi.array()
 })
@@ -15,6 +39,6 @@ module.exports.cargoSchema = Joi.object({
 module.exports.bidSchema = Joi.object({
     bid: Joi.object({
         amount: Joi.number().required().min(10),
-        message: Joi.string().required()
+        message: Joi.string().required().escapeHTML()
     }).required()
 })
