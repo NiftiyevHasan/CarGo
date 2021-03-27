adc2f88ce08da513722c514db1de5026c5b359e5
Hasan Niftiyev <ditdo@Ditdos-MacBook-Pro.local>
2021-03-26 18:25:22 -0700

    fixed scroll-up button show up on every page

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -39,4 +39,4 @@ function generateCargo(cargo) {
 
 
 
-    
+
--- public/javascripts/scrollToTop.js
+++ public/javascripts/scrollToTop.js
@@ -3,6 +3,9 @@ var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
 var rootElement = document.documentElement
 
 window.onload = function () {
+    if(this.document.location.pathname == '/cargopanel'){
+        scrollToTopBtn.classList.remove('invisible')
+    }
     scrollToTopBtn.addEventListener('click', () => {
         rootElement.scrollTo({
             top: 0,
